11ddaf564fec061703a8dc011d3df1fa8065961c
martin <martin.clemons@mac.com>
2014-03-30 00:15:47 -0600

    add counting Semaphores to library.

---
+++ semaphore.c
@@ -0,0 +1,41 @@
+/*! \file
+ *  semaphore.c
+ *  wireless-sensor-02.1
+ *
+ * Copyright (c) 2014 Martin Clemons
+ *
+ */
+
+"semaphore.h"
+
+
+
+/***                Definitions                 ***/
+/*! \privatesection */
+
+
+
+/***         Private Global Variables           ***/
+
+
+
+/***          Public Global Variables           ***/
+/*! \publicsection */
+
+
+
+/***        Private function prototypes         ***/
+/*! \privatesection */
+
+
+
+/***                   ISRs                     ***/
+
+
+
+/***             Private Functions              ***/
+
+
+
+/***             Public Functions               ***/
+/*! \publicsection */
---
+++ semaphore.h
@@ -0,0 +1,21 @@
+/*! \file
+ *  semaphore.h
+ *  wireless-sensor-02.1
+ *
+ * Copyright (c) 2014 Martin Clemons
+ *
+ */
+
+#pragma once
+
+<inttypes.h>
+<avr/io.h>
+
+
+
+/***            Public Variables            ***/
+
+
+
+/***            Public Functions            ***/
+
