d25c4b1b042574ec887720cd557232944318e373
martin <martin.clemons@mac.com>
2015-08-25 20:58:35 -0600

    Update formatting, add empty function as defualt ISR rather than null pointer.

--- dma.c
+++ dma.c
@@ -38,12 +38,15 @@
 
 /***         Private Global Variables           ***/
 
-/* pointers to external DMA channel interrupt functions */
+/* pointers to external DMA channel interrupt functions.
+ * Initially set to empty function for safety.
+ */
 #ifdef XENON_DMA_LIB_ISR_HANDLER
-void (*ch0Isr)(void) = 0;
-void (*ch1Isr)(void) = 0;
-void (*ch2Isr)(void) = 0;
-void (*ch3Isr)(void) = 0;
+void empty(void) {}
+void (*ch0Isr)(void) = empty;
+void (*ch1Isr)(void) = empty;
+void (*ch2Isr)(void) = empty;
+void (*ch3Isr)(void) = empty;
 #endif /* XENON_DMA_LIB_ISR_HANDLER */
 
 
@@ -59,26 +62,26 @@ void (*ch3Isr)(void) = 0;
 /***                   ISRs                     ***/
 
 #ifdef XENON_DMA_LIB_ISR_HANDLER
-ISR(DMA_CH0_vect) {
-    
+ISR(DMA_CH0_vect)
+{
     ch0Isr();
     DMA.CH0.CTRLB |= DMA_CH_TRNIF_bm;
 }
 
-ISR(DMA_CH1_vect) {
-    
+ISR(DMA_CH1_vect)
+{
     ch1Isr();
     DMA.CH1.CTRLB |= DMA_CH_TRNIF_bm;
 }
 
-ISR(DMA_CH2_vect) {
-    
+ISR(DMA_CH2_vect)
+{
     ch2Isr();
     DMA.CH2.CTRLB |= DMA_CH_TRNIF_bm;
 }
 
-ISR(DMA_CH3_vect) {
-    
+ISR(DMA_CH3_vect)
+{
     ch3Isr();
     DMA.CH3.CTRLB |= DMA_CH_TRNIF_bm;
 }
@@ -95,24 +98,24 @@ ISR(DMA_CH3_vect) {
  *  \param dbm Double buffer mode selection.
  *  \param cp Channel priority selection.
  */
-void dmaInit(enum dmaDoubleBufferMode_e dbm, enum dmaChannelPriority_e cp) {
-    
+void dmaInit(enum dmaDoubleBufferMode_e dbm, enum dmaChannelPriority_e cp)
+{
     DMA.CTRL = dbm | cp;
 }
 
 /*! Enable DMA controller
  *
  */
-void dmaEnable(void) {
-    
+void dmaEnable(void)
+{
     DMA.CTRL |= DMA_ENABLE_bm;
 }
 
 /*! Disable DMA controller
  *
  */
-void dmaDisable(void) {
-    
+void dmaDisable(void)
+{
     DMA.CTRL &= ~(DMA_ENABLE_bm);
 }
 
@@ -131,8 +134,8 @@ void dmaChInitSingleShot(struct DMA_CH_struct *c,
                          enum dmaChAddressMode_e srcDir,
                          enum dmaChAddressReload_e destReload,
                          enum dmaChAddressMode_e destDir,
-                         enum DMA_CH_TRIGSRC_enum tSource) {
-    
+                         enum DMA_CH_TRIGSRC_enum tSource)
+{
     // configure DMA channel registers
     c->CTRLA = DMA_CH_SINGLE_bm | burstLen;
     c->CTRLB = DMA_CH_ERRIF_bm | DMA_CH_TRNIF_bm;
@@ -150,8 +153,8 @@ void dmaChInitSingleShot(struct DMA_CH_struct *c,
 #ifdef XENON_DMA_LIB_ISR_HANDLER
 void dmaChRegisterIsr(struct DMA_CH_struct *c, 
                       enum dmaChTransactionCompleteInterrupt_e cmpIntLvl,
-                      void (*isr)(void)) {
-    
+                      void (*isr)(void))
+{
     // configure interrupt service routine
     c->CTRLB &= ~DMA_CH_TRNINTLVL_HI_gc;        // clear previous bits
     if (isr != NULL) {
@@ -181,8 +184,8 @@ void dmaChRegisterIsr(struct DMA_CH_struct *c,
  *  \param c Pointer to DMA channel to configure.
  *  \param repCount Value to set DMA channel REPCNT register.
  */
-void dmaChEnableRepeat(struct DMA_CH_struct *c, uint8_t repCount) {
-    
+void dmaChEnableRepeat(struct DMA_CH_struct *c, uint8_t repCount)
+{
     c->REPCNT = repCount;
     c->CTRLA |= DMA_CH_REPEAT_bm;
 }
@@ -200,8 +203,8 @@ void dmaChSetRepeatCount(struct DMA_CH_struct *c, uint16_t count)
  *  \param c Pointer to DMA channel for which source address will be set.
  *  \param source Pointer to DMA channel source.
  */
-void dmaChSetSource(struct DMA_CH_struct *c, void *source) {
-    
+void dmaChSetSource(struct DMA_CH_struct *c, void *source)
+{
     ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
         c->SRCADDR0 = (uint8_t)((uint16_t)source);      // low byte of address
         c->SRCADDR1 = (uint8_t)((uint16_t)source >> 8); // high byte of address
@@ -213,8 +216,8 @@ void dmaChSetSource(struct DMA_CH_struct *c, void *source) {
  *  \param c Pointer to DMA channel for which destination address will be set.
  *  \param dest Pointer to DMA channel destination.
  */
-void dmaChSetDestination(struct DMA_CH_struct *c, volatile void *dest) {
-    
+void dmaChSetDestination(struct DMA_CH_struct *c, volatile void *dest)
+{
     ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
         c->DESTADDR0 = (uint8_t)((uint16_t)dest);      // low byte of address
         c->DESTADDR1 = (uint8_t)((uint16_t)dest >> 8); // high byte of address
@@ -226,8 +229,8 @@ void dmaChSetDestination(struct DMA_CH_struct *c, volatile void *dest) {
  *  \param c Pointer to DMA channel for which transfer count will be set.
  *  \param count Transfer count.
  */
-void dmaChSetTransferCount(struct DMA_CH_struct *c, uint16_t count) {
-    
+void dmaChSetTransferCount(struct DMA_CH_struct *c, uint16_t count)
+{
     /* TEMP registers are shared between ALL DMA channels */
     ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
         c->TRFCNT = count;
@@ -238,8 +241,8 @@ void dmaChSetTransferCount(struct DMA_CH_struct *c, uint16_t count) {
  *  \param c Pointer to DMA channel for which transfer count will be returned.
  *  \return Returns value of DMA channel transfer count register.
  */
-uint16_t dmaChGetTransferCount(struct DMA_CH_struct *c) {
-    
+uint16_t dmaChGetTransferCount(struct DMA_CH_struct *c)
+{
     uint16_t count;
     /* TEMP registers are shared between ALL DMA channels */
     ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
@@ -251,16 +254,16 @@ uint16_t dmaChGetTransferCount(struct DMA_CH_struct *c) {
 /*! Enable DMA channel.
  *  \param c Pointer to DMA channel to enable.
  */
-void dmaChEnable(struct DMA_CH_struct *c) {
-    
+void dmaChEnable(struct DMA_CH_struct *c)
+{
     c->CTRLA |= DMA_CH_ENABLE_bm;
 }
 
 /*! Disable DMA channel.
  *  \param c Pointer to DMA channel to disable.
  */
-void dmaChDisable(struct DMA_CH_struct *c) {
-    
+void dmaChDisable(struct DMA_CH_struct *c)
+{
     c->CTRLA &= ~DMA_CH_ENABLE_bm;
 }
 
@@ -268,8 +271,8 @@ void dmaChDisable(struct DMA_CH_struct *c) {
  *  \return Returns 1 if channel is enabled and transaction has not yet completed, 0 if channel is
  *  not enabled.
  */
-uint8_t dmaChActive(struct DMA_CH_struct *c) {
-    
+uint8_t dmaChActive(struct DMA_CH_struct *c)
+{
     return c->CTRLA & DMA_CH_ENABLE_bm;
 }
 
