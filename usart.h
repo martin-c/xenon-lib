4cffe18fac0fa88eb87c8d19ba587e4384c2cdb0
martin <martin.clemons@mac.com>
2015-09-02 18:08:57 -0600

    move USART ISR conditional compilation flag out of usart header and into common library configuration file.

--- usart.h
+++ usart.h
@@ -30,15 +30,10 @@
 #include <inttypes.h>
 #include <avr/io.h>
 #include <avr/pgmspace.h>
+#include "lib-config.h"
 #include "task-scheduler.h"
 #include "embedded-device.h"
 
-/* Defines whether USART lib should implement interrupt handlers for USART
- * DRE/RXC/TXC interrupts or if these will be implemented by user.
- * If USART_LIB_ISR_HANDLER is defined, then DRE/RXC/TXC interrupts will be
- * handled by USART lib and `usartIsr*` functions and related will be available.
- */
-//#define USART_LIB_ISR_HANDLER 1
 
 
 /***            Public Variables            ***/
